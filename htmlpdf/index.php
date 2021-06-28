ec349c3a91a33d83f4a5b2af972e042c2f6b1e3f
muafasyarifudin <33217217+muafasyarifudin@users.noreply.github.com>
2021-06-28 09:08:15 +0700

    Update index.php

--- htmlpdf/index.php
+++ htmlpdf/index.php
@@ -1,7 +1,7 @@
 <?php
 	$html = ob_get_contents();
 	ob_end_clean();
-	require __DIR__.'(masukkan direktori halaman cetak)';
+	require __DIR__.'(masukkan direktori library)';
 	use Spipu\Html2Pdf\Html2Pdf;
 	$html2pdf = new Html2Pdf('L','A4','fr', true, 'UTF-8', array(15, 15, 15, 15), false); 
 	$html2pdf->writeHTML($html);
